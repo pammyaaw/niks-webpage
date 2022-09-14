36ba33933d67826515270c844029af2d4e809c2e
pammyaaw <110321789+pammyaaw@users.noreply.github.com>
2022-09-13 22:33:35 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomImageSource1 = "dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = " Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = " Player 2 Wins!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
